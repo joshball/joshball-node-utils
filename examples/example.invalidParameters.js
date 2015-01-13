b2d2c5f4554a47bf90e8ad41ae1b035193eaa45b
Joshua Ball <joshua.g.s.ball@gmail.com>
2015-01-13 10:20:06 -0800

    Initial checkin. InvalidParameters and checkNotNull

---
+++ examples/example.invalidParameters.js
@@ -0,0 +1,24 @@
+'use strict';
+
+var cl = require('contralog')(module);
+cl.on();
+var Utils = require('../lib');
+
+var invalidParameters = function (some, params) {
+    cl.trace('invalidParameters');
+    Utils.assertions.checkNotNull(some);
+    Utils.assertions.checkNotNull(params);
+};
+
+var sub = function(){
+    cl.trace('sub');
+    invalidParameters('some');
+};
+
+var main = function(){
+    cl.trace('main');
+    sub();
+};
+
+
+main();
